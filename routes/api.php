1f4a5dc045c2bd2aff1cd7171db62ba00ae7d593
--global <--global>
2025-05-02 13:14:11 +0300

    configuration bootsstrap/app.php puis configuration root, api rest v1 OK

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+use App\Http\Controllers\AppartementController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('appartements', action:  [AppartementController::class, 'index']);
+Route::post('appartements', action:  [AppartementController::class, 'store']);
+Route::delete('appartement/{id}', action: [AppartementController::class, 'destroy']);
+Route::put('appartement/{id}', action: [AppartementController::class, 'update']);
